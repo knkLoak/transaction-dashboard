1d104b6cc027efb4519a1d43c979178adb814431
knkLoak <96754154+knkLoak@users.noreply.github.com>
2025-07-30 16:18:50 -0400

    Delete .scripts directory

--- .scripts/push-to-hf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This script is not committed to GitHub or Hugging Face
-git remote add huggingface https://user:${HF_TOKEN}@huggingface.co/spaces/knkLoak/transaction-dashboard
-git push huggingface main --force
